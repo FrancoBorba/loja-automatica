33a508b687ddb8c5235a3b479ad061a3a40dd366
Franco Borba <franco.borba14@gmail.com>
2025-07-10 17:35:23 -0300

    Initial Spring Boot project for Automatic Store

---
+++ src/main/java/br/uesb/cipec/loja_automatica/LojaAutomaticaApplication.java
@@ -0,0 +1,13 @@
+package br.uesb.cipec.loja_automatica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaAutomaticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaAutomaticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/uesb/cipec/loja_automatica/LojaAutomaticaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uesb.cipec.loja_automatica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaAutomaticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
