aa18e12d0027d956e01a2041d5d5234941bf36a8
FrancoBorba <franco.borba14@gmail.com>
2025-07-28 16:29:19 -0300

    Adding documentaion with swagger

--- src/main/java/br/uesb/cipec/loja_automatica/DTO/PurchaseRequestDTO.java
+++ src/main/java/br/uesb/cipec/loja_automatica/DTO/PurchaseRequestDTO.java
@@ -5,20 +5,26 @@ import java.util.List;
 
 import br.uesb.cipec.loja_automatica.enums.StatusPurchase;
 import br.uesb.cipec.loja_automatica.enums.TypePayment;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
+@Schema(description = "DTO for creating a purchase")
 public class PurchaseRequestDTO {
 
   @NotNull(message = "Purchase status is required")
+  
+  @Schema(description = "Status purchase", example = "PAGO")
   private StatusPurchase statusPurchase;
   
   
   @NotNull(message = "Payment is mandatory")
+    @Schema(description = "Type of payment", example = "DEBITO")
   private TypePayment payment;
 
 
+  @Schema(description = "Item lists")
   @NotEmpty(message = "The item List cannot be empty")
   @Valid // Validates the items since it is internal
   private List<ItemPurchaseRequestDTO> itens;
